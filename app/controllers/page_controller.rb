87448ed258bcaad345f73b07574b1cac9395b881
EC2 Default User <ec2-user@ip-172-31-46-205.us-east-2.compute.internal>
2018-08-19 19:08:22 +0000

    css almost over

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -6,6 +6,12 @@ class PageController < ApplicationController
   end
 
   def home_page
+    @reviews= Review.all
+    @questions=Question.all
+    @infos=Info.all
+    @matchings=Matching.all
+    @clubs=Club.all
+    @count=0
   end
 
   def save_page
